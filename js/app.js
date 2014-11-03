a49abde2919b9a2b6f482b2533b0e2bf832d2660
ayushsoni <soniayush1@yahoo.com>
2014-11-03 12:15:12 -0800

    CLASS

--- js/app.js
+++ js/app.js
@@ -5,3 +5,25 @@
 
 "use strict";
 
+//create a new angular module- everything in angular go into modules; avoids creating global variables
+//function is where we declare our states
+angular.module('MovieApp', [])
+    .controller('MoviesController', function($scope) {
+        //create a new property on the scope
+        $scope.movies = movies;
+        $scope.searchString = '';
+        $scope.sortCol = 'rank';
+
+        //adding new key and value pair
+        $scope.sortReverse = false;
+
+        $scope.sortBy = function(colName) {
+            if ($scope.sortCol == colName) {
+                $scope.sortReverse = !$scope.sortReverse;
+            }
+            else {
+                $scope.sortReverse = false;
+            }
+            $scope.sortCol = colName;
+        };
+    });
